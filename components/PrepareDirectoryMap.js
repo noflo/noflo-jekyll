76c81a01915d14eef0b2ae60b73b4c968c166903
Henri Bergius <henri.bergius@iki.fi>
2017-11-13 19:12:54 +0100

    Make linter happy

--- components/PrepareDirectoryMap.js
+++ components/PrepareDirectoryMap.js
@@ -1,18 +1,25 @@
 const noflo = require('noflo');
 
-exports.getComponent = function() {
-  const c = new noflo.Component;
-  c.inPorts.add('from',
-    {datatype: 'string'});
-  c.inPorts.add('to',
-    {datatype: 'string'});
-  c.outPorts.add('out');
-  c.forwardBrackets =
-    {to: ['out']};
-  return c.process(function(input, output) {
+exports.getComponent = () => {
+  const c = new noflo.Component();
+  c.inPorts.add('from', {
+    datatype: 'string',
+  });
+  c.inPorts.add('to', {
+    datatype: 'string',
+  });
+  c.outPorts.add('out', {
+    datatype: 'string',
+  });
+  c.forwardBrackets = {
+    to: ['out'],
+  };
+  c.process((input, output) => {
     if (!input.has('from', 'to')) { return; }
     const values = input.getData('from', 'to');
-    return output.sendDone({
-      out: values.join('=')});
+    output.sendDone({
+      out: values.join('='),
+    });
   });
+  return c;
 };
